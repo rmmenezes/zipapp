972d1acf1eff2d0e726ca8ada884ebbd731927c7
Rafael Menezes Barboza <ra29fa@gmail.com>
2022-01-25 01:32:51 -0300

    .

--- lib/view/profile_page.dart
+++ lib/view/profile_page.dart
@@ -25,6 +25,8 @@ class _ProfilePageState extends State<ProfilePage> {
   TextEditingController nameController = TextEditingController();
   late var photoChanged;
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     editable = false;
@@ -45,151 +47,165 @@ class _ProfilePageState extends State<ProfilePage> {
       appBar: MyAppBar(title: "Perfil"),
       body: Padding(
         padding: const EdgeInsets.all(25.0),
-        child: Column(children: [
-          InkWell(
-            onTap: () async {
-              if (editable) {
-                await showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                            content: const Text("Escolha a fonte da imagem"),
-                            actions: [
-                              InkWell(
-                                  child: const Text("Câmera"),
-                                  onTap: () async {
-                                    PickedFile? pickedFile =
-                                        await StudentController()
-                                            .selectInputImage("camera");
-                                    setState(() {
-                                      photo = pickedFile!.path;
-                                      photoChanged = pickedFile;
-                                    });
-                                    Navigator.pop(context);
-                                  }),
-                              InkWell(
-                                  child: const Text("Galeria"),
-                                  onTap: () async {
-                                    PickedFile? pickedFile =
-                                        await StudentController()
-                                            .selectInputImage("gallery");
-                                    setState(() {
-                                      photo = pickedFile!.path;
-                                      photoChanged = pickedFile;
-                                    });
-                                    Navigator.pop(context);
-                                  }),
-                            ]));
-              }
-            },
-            child: Container(
-              padding: const EdgeInsets.all(20.0),
-              width: 170.0,
-              height: 170.0,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    fit: BoxFit.cover, image: NetworkImage(photo)),
-                boxShadow: [
-                  editable == true
-                      ? const BoxShadow(color: Colors.orange, spreadRadius: 4)
-                      : const BoxShadow(color: Colors.orange, spreadRadius: 0),
-                ],
-                borderRadius: const BorderRadius.all(Radius.circular(100.0)),
+        child: Form(
+          key: _formKey,
+          child: Column(children: [
+            InkWell(
+              onTap: () async {
+                if (editable) {
+                  await showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                              content: const Text("Escolha a fonte da imagem"),
+                              actions: [
+                                InkWell(
+                                    child: const Text("Câmera"),
+                                    onTap: () async {
+                                      PickedFile? pickedFile =
+                                          await StudentController()
+                                              .selectInputImage("camera");
+                                      setState(() {
+                                        photo = pickedFile!.path;
+                                        photoChanged = pickedFile;
+                                      });
+                                      Navigator.pop(context);
+                                    }),
+                                InkWell(
+                                    child: const Text("Galeria"),
+                                    onTap: () async {
+                                      PickedFile? pickedFile =
+                                          await StudentController()
+                                              .selectInputImage("gallery");
+                                      setState(() {
+                                        photo = pickedFile!.path;
+                                        photoChanged = pickedFile;
+                                      });
+                                      Navigator.pop(context);
+                                    }),
+                              ]));
+                }
+              },
+              child: Container(
+                padding: const EdgeInsets.all(20.0),
+                width: 170.0,
+                height: 170.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover, image: NetworkImage(photo)),
+                  boxShadow: [
+                    editable == true
+                        ? const BoxShadow(color: Colors.orange, spreadRadius: 4)
+                        : const BoxShadow(
+                            color: Colors.orange, spreadRadius: 0),
+                  ],
+                  borderRadius: const BorderRadius.all(Radius.circular(100.0)),
+                ),
               ),
             ),
-          ),
-          InkWell(
-            onTap: () {
-              setState(() {
-                editable = !editable;
-              });
-            },
-            child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: const [
-                  Icon(Icons.edit, size: 20),
-                  SizedBox(width: 10),
-                  Text("Editar")
-                ]),
-          ),
-          const SizedBox(height: 40),
-          TextFormField(
-            controller: nameController,
-            enabled: editable,
-            keyboardType: TextInputType.name,
-            textInputAction: TextInputAction.done,
-            decoration: const InputDecoration(
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.orange, width: 1),
-                ),
-                labelText: 'Nome Completo:',
-                icon: Icon(Icons.account_circle_outlined)),
-          ),
-          const SizedBox(height: 20),
-          TextFormField(
-            enabled: false,
-            initialValue: widget.student.email,
-            keyboardType: TextInputType.emailAddress,
-            textInputAction: TextInputAction.done,
-            decoration: const InputDecoration(
-                labelText: 'Email:', icon: Icon(Icons.email)),
-          ),
-          const SizedBox(height: 20),
-          TextFormField(
-            enabled: false,
-            initialValue: widget.student.barcode,
-            keyboardType: TextInputType.number,
-            textInputAction: TextInputAction.done,
-            decoration: const InputDecoration(
-                labelText: 'Número:', icon: Icon(Icons.format_list_numbered)),
-          ),
-          const SizedBox(height: 20),
-          editable == true
-              ? buttonGerator(
-                  backgroundColor: Colors.orange.shade200,
-                  text: "Salvar Edição",
-                  onClickFuncion: () async {
-                    try {
-                      Loader.show(context,
-                          progressIndicator: const LinearProgressIndicator());
-                      //default
+            InkWell(
+              onTap: () {
+                setState(() {
+                  editable = !editable;
+                });
+              },
+              child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: const [
+                    Icon(Icons.edit, size: 20),
+                    SizedBox(width: 10),
+                    Text("Editar")
+                  ]),
+            ),
+            const SizedBox(height: 40),
+            TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Preencha com seu Nome Completo';
+                }
+                return null;
+              },
+              controller: nameController,
+              enabled: editable,
+              keyboardType: TextInputType.name,
+              textInputAction: TextInputAction.done,
+              decoration: const InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.orange, width: 1),
+                  ),
+                  labelText: 'Nome Completo:',
+                  icon: Icon(Icons.account_circle_outlined)),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              enabled: false,
+              initialValue: widget.student.email,
+              keyboardType: TextInputType.emailAddress,
+              textInputAction: TextInputAction.done,
+              decoration: const InputDecoration(
+                  labelText: 'Email:', icon: Icon(Icons.email)),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              enabled: false,
+              initialValue: widget.student.barcode,
+              keyboardType: TextInputType.number,
+              textInputAction: TextInputAction.done,
+              decoration: const InputDecoration(
+                  labelText: 'Número:', icon: Icon(Icons.format_list_numbered)),
+            ),
+            const SizedBox(height: 20),
+            editable == true
+                ? buttonGerator(
+                    backgroundColor: Colors.orange.shade200,
+                    text: "Salvar Edição",
+                    onClickFuncion: () async {
+                      if (_formKey.currentState!.validate()) {
+                        try {
+                          Loader.show(context,
+                              progressIndicator:
+                                  const LinearProgressIndicator());
+                          //default
 
-                      if (photoChanged != null) {
-                        await StudentController().updateStudent(
-                            widget.student.uid, nameController.text);
-                        await StudentController().uploadProfilePicure(
-                            widget.student.uid, photoChanged);
-                      } else {
-                        await StudentController().updateStudent(
-                            widget.student.uid, nameController.text);
+                          if (photoChanged != null) {
+                            await StudentController().updateStudent(
+                                widget.student.uid, nameController.text);
+                            await StudentController().uploadProfilePicure(
+                                widget.student.uid, photoChanged);
+                          } else {
+                            await StudentController().updateStudent(
+                                widget.student.uid, nameController.text);
+                          }
+
+                          Loader.hide();
+
+                          // confirma
+                          await CoolAlert.show(
+                              loopAnimation: false,
+                              context: context,
+                              type: CoolAlertType.success,
+                              text: "Editado com Sucesso!!");
+                          StudentModel studentAsModel =
+                              await StudentController()
+                                  .getStudentAsModel(widget.student.uid);
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      HomePage(student: studentAsModel)));
+                        } on Exception {
+                          // erro
+                          await CoolAlert.show(
+                              loopAnimation: false,
+                              context: context,
+                              type: CoolAlertType.error,
+                              text: "Erro!!");
+                        }
                       }
-
-                      Loader.hide();
-
-                      // confirma
-                      await CoolAlert.show(
-                          loopAnimation: false,
-                          context: context,
-                          type: CoolAlertType.success,
-                          text: "Editado com Sucesso!!");
-                      StudentModel studentAsModel = await StudentController()
-                          .getStudentAsModel(widget.student.uid);
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  HomePage(student: studentAsModel)));
-                    } on Exception {
-                      // erro
-                      await CoolAlert.show(
-                          loopAnimation: false,
-                          context: context,
-                          type: CoolAlertType.error,
-                          text: "Erro!!");
-                    }
-                  })
-              : const SizedBox(),
-        ]),
+                    })
+                : const SizedBox(),
+          ]),
+        ),
       ),
     );
   }
--- lib/view/register_student_page.dart
+++ lib/view/register_student_page.dart
@@ -36,6 +36,8 @@ class _RegisterStudentPageState extends State<RegisterStudentPage> {
   TextEditingController emailController = TextEditingController();
   TextEditingController barcodeController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     if (widget.photo != null) {
@@ -56,129 +58,152 @@ class _RegisterStudentPageState extends State<RegisterStudentPage> {
         appBar: MyAppBar(title: "Registro"),
         body: Padding(
           padding: const EdgeInsets.all(25.0),
-          child: Column(children: [
-            InkWell(
-              onTap: () async {
-                await showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                            content: const Text("Escolha a fonte da imagem"),
-                            actions: [
-                              InkWell(
-                                  child: const Text("Câmera"),
-                                  onTap: () async {
-                                    PickedFile? pickedFile =
-                                        await StudentController()
-                                            .selectInputImage("camera");
-                                    setState(() {
-                                      photo = pickedFile!.path;
-                                      photoChanged = pickedFile;
-                                    });
-                                    Navigator.pop(context);
-                                  }),
-                              InkWell(
-                                  child: const Text("Galeria"),
-                                  onTap: () async {
-                                    PickedFile? pickedFile =
-                                        await StudentController()
-                                            .selectInputImage("gallery");
-                                    setState(() {
-                                      photo = pickedFile!.path;
-                                      photoChanged = pickedFile;
-                                    });
-                                    Navigator.pop(context);
-                                  }),
-                            ]));
-              },
-              child: Container(
-                padding: const EdgeInsets.all(20.0),
-                width: 170.0,
-                height: 170.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      fit: BoxFit.cover, image: NetworkImage(photo)),
-                  boxShadow: const [
-                    BoxShadow(color: Colors.orange, spreadRadius: 4),
-                  ],
-                  borderRadius: const BorderRadius.all(Radius.circular(100.0)),
+          child: Form(
+            key: _formKey,
+            child: Column(children: [
+              InkWell(
+                onTap: () async {
+                  await showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                              content: const Text("Escolha a fonte da imagem"),
+                              actions: [
+                                InkWell(
+                                    child: const Text("Câmera"),
+                                    onTap: () async {
+                                      PickedFile? pickedFile =
+                                          await StudentController()
+                                              .selectInputImage("camera");
+                                      setState(() {
+                                        photo = pickedFile!.path;
+                                        photoChanged = pickedFile;
+                                      });
+                                      Navigator.pop(context);
+                                    }),
+                                InkWell(
+                                    child: const Text("Galeria"),
+                                    onTap: () async {
+                                      PickedFile? pickedFile =
+                                          await StudentController()
+                                              .selectInputImage("gallery");
+                                      setState(() {
+                                        photo = pickedFile!.path;
+                                        photoChanged = pickedFile;
+                                      });
+                                      Navigator.pop(context);
+                                    }),
+                              ]));
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(20.0),
+                  width: 170.0,
+                  height: 170.0,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.cover, image: NetworkImage(photo)),
+                    boxShadow: const [
+                      BoxShadow(color: Colors.orange, spreadRadius: 4),
+                    ],
+                    borderRadius:
+                        const BorderRadius.all(Radius.circular(100.0)),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 40),
-            TextFormField(
-              inputFormatters: [LengthLimitingTextInputFormatter(100)],
-              controller: nameController,
-              keyboardType: TextInputType.name,
-              textInputAction: TextInputAction.done,
-              decoration: const InputDecoration(
-                  labelText: 'Nome Completo:',
-                  icon: Icon(Icons.account_circle_outlined)),
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              inputFormatters: [LengthLimitingTextInputFormatter(100)],
-              controller: emailController,
-              keyboardType: TextInputType.emailAddress,
-              textInputAction: TextInputAction.done,
-              decoration: const InputDecoration(
-                  labelText: 'Email:', icon: Icon(Icons.email)),
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              inputFormatters: [LengthLimitingTextInputFormatter(4)],
-              controller: barcodeController,
-              keyboardType: TextInputType.number,
-              textInputAction: TextInputAction.done,
-              decoration: const InputDecoration(
-                  labelText: 'Número:', icon: Icon(Icons.format_list_numbered)),
-            ),
-            const SizedBox(height: 20),
-            buttonGerator(
-                text: "Confirmar Cadastro",
-                onClickFuncion: () async {
-                  try {
-                    // registra
-                    StudentModel student;
-                    if (photoChanged != null) {
-                      student = await StudentController().registerStudent(
-                          widget.uid.toString(),
-                          nameController.text,
-                          emailController.text,
-                          "",
-                          barcodeController.text);
-                      await StudentController()
-                          .uploadProfilePicure(student.uid, photoChanged);
-                    } else {
-                      student = await StudentController().registerStudent(
-                          widget.uid.toString(),
-                          nameController.text,
-                          emailController.text,
-                          photo,
-                          barcodeController.text);
-                    }
-                    // confirma
-                    await CoolAlert.show(
-                        loopAnimation: false,
-                        context: context,
-                        type: CoolAlertType.success,
-                        text: "Registrado com Sucesso!!");
-                    // go home page
-                    StudentModel studentAsModel = await StudentController()
-                        .getStudentAsModel(student.uid);
-                    await Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                HomePage(student: studentAsModel)));
-                  } on Exception {
-                    await CoolAlert.show(
-                        loopAnimation: false,
-                        context: context,
-                        type: CoolAlertType.error,
-                        text: "Erro!!");
+              const SizedBox(height: 40),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Preencha com seu Nome Completo';
                   }
-                })
-          ]),
+                  return null;
+                },
+                inputFormatters: [LengthLimitingTextInputFormatter(100)],
+                controller: nameController,
+                keyboardType: TextInputType.name,
+                textInputAction: TextInputAction.done,
+                decoration: const InputDecoration(
+                    labelText: 'Nome Completo:',
+                    icon: Icon(Icons.account_circle_outlined)),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                enabled: false,
+                inputFormatters: [LengthLimitingTextInputFormatter(100)],
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.done,
+                decoration: const InputDecoration(
+                    labelText: 'Email:', icon: Icon(Icons.email)),
+              ),
+              const SizedBox(height: 20),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Preencha com seu número da Zip';
+                  }
+                  return null;
+                },
+                controller: barcodeController,
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.digitsOnly,
+                  LengthLimitingTextInputFormatter(4)
+                ],
+                textInputAction: TextInputAction.done,
+                decoration: const InputDecoration(
+                    labelText: 'Número:',
+                    icon: Icon(Icons.format_list_numbered)),
+              ),
+              const SizedBox(height: 20),
+              buttonGerator(
+                  text: "Confirmar Cadastro",
+                  onClickFuncion: () async {
+                    if (_formKey.currentState!.validate()) {
+                      try {
+                        // registra
+                        StudentModel student;
+                        if (photoChanged != null) {
+                          student = await StudentController().registerStudent(
+                              widget.uid.toString(),
+                              nameController.text,
+                              emailController.text,
+                              "",
+                              barcodeController.text);
+                          await StudentController()
+                              .uploadProfilePicure(student.uid, photoChanged);
+                        } else {
+                          student = await StudentController().registerStudent(
+                              widget.uid.toString(),
+                              nameController.text,
+                              emailController.text,
+                              photo,
+                              barcodeController.text);
+                        }
+                        // confirma
+                        await CoolAlert.show(
+                            loopAnimation: false,
+                            context: context,
+                            type: CoolAlertType.success,
+                            text: "Registrado com Sucesso!!");
+                        // go home page
+                        StudentModel studentAsModel = await StudentController()
+                            .getStudentAsModel(student.uid);
+                        await Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    HomePage(student: studentAsModel)));
+                      } on Exception {
+                        await CoolAlert.show(
+                            loopAnimation: false,
+                            context: context,
+                            type: CoolAlertType.error,
+                            text: "Erro!!");
+                      }
+                    }
+                  })
+            ]),
+          ),
         ));
   }
 }
